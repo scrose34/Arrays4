be1ac9c966a7f65f77287c1e305c88f3ba2e3430
Spencer <scrose34@gmail.com>
2021-02-05 20:04:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Element Value 1");
+            Console.WriteLine("Element Value 2");
+            Console.WriteLine("Element Value 3");
+            Console.WriteLine("Element Value 4");
+            Console.WriteLine("Element Value 5");
+            Console.WriteLine("Element Value 6");
+            Console.WriteLine("Element Value 7");
+            Console.WriteLine("Element Value 8");
+            Console.WriteLine("Element Value 9");
+            Console.WriteLine("Element Value 10");
+            Console.WriteLine("Element Value 11");
+            Console.WriteLine("Element Value 12");
+            Console.WriteLine("Element Value 13");
+            Console.WriteLine("Element Value 14");
+            Console.WriteLine("Element Value 15");
+            Console.WriteLine("Element Value 16");
+            Console.WriteLine("Element Value 17");
+            Console.WriteLine("Element Value 18");
+            Console.WriteLine("Element Value 19");
+            Console.WriteLine("Element Value 20");
+            Console.WriteLine("Element Value 21");
+            Console.WriteLine("Element Value 22");
+            Console.WriteLine("Element Value 23");
+            Console.WriteLine("Element Value 24");
+            Console.WriteLine("Element Value 25");
+        }
+    }
+}
